d9f1c6925be0e110ac2f271c52fb805b7bad593d
JakeTheCorn <jakethecorn@gmail.com>
2020-08-18 20:37:48 -0400

    add ability to handle multiple extensions

--- cmd.js
+++ cmd.js
@@ -1,21 +1,32 @@
 const Utils = require('./utils')
+const utils = new Utils()
 
 main()
 
 async function main() {
-    await all('./')
+    await all({
+        path: './',
+        patterns: [
+            '.ut.js',
+            '.test.js'
+        ],
+    })
 }
 
-async function all(path) {
-    const testFiles = await getTestFilesNames(path)
+async function all({ path, patterns }) {
+    const testFiles = await getTestFilesNames({
+        path,
+        patterns
+    })
     for (const testFile of testFiles) {
         require(testFile)
     }
     require('./unittest').main()
 }
 
-async function getTestFilesNames(path = './') {
-    const utils = new Utils()
+
+
+async function getTestFilesNames({ path, patterns }) {
     const testFiles = []
     const files = await utils.readdir(path)
     for (const file of files) {
@@ -24,11 +35,17 @@ async function getTestFilesNames(path = './') {
             continue
         }
         if (utils.isDirectory(f)) {
-            const files = await getTestFilesNames(f + '/')
+            const files = await getTestFilesNames({
+                path: f + '/',
+                patterns,
+            })
             testFiles.push(...files)
         }
-        if (/\.test\.js/.test(f)) {
-            testFiles.push(f)
+        const regexs = patterns.map(p => new RegExp(p))
+        for (const regex of regexs) {
+            if (regex.test(f)) {
+                testFiles.push(f)
+            }
         }
     }
     return testFiles
---
+++ extension.ut.js
@@ -0,0 +1,16 @@
+const unittest = require('./unittest')
+
+class SubdirTests extends unittest.TestCase {
+    test() {
+        this.assertEqual(7, 7)
+    }
+}
+
+unittest
+  .register(
+      SubdirTests
+  )
+
+if (require.main === module) {
+    unittest.main()
+}
