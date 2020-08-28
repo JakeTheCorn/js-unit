182552919d647a52c04881d038e4dd1e780d09f1
JakeTheCorn <jakethecorn@gmail.com>
2020-08-28 14:00:28 -0400

    leave todo

--- lib_tests/datetime.test.js
+++ lib_tests/datetime.test.js
@@ -1,28 +1,5 @@
 const unittest = require('../unittest')
-
-
-class DateTime {
-    constructor(date) {
-        this.date = date
-        this.__throw_for_invalid_fields()
-    }
-
-    years_equal(other) {
-        if (!(other instanceof DateTime)) {
-            throw new TypeError('years_equal must be called with instance of DateTime')
-        }
-        const other_full_year = other.date.getFullYear()
-        const this_full_year = this.date.getFullYear()
-
-        return other_full_year === this_full_year
-    }
-
-    __throw_for_invalid_fields() {
-        if (!(this.date instanceof Date)) {
-            throw new TypeError()
-        }
-    }
-}
+const DateTime = require('../lib/datetime')
 
 
 class DateTimeTests extends unittest.TestCase {
