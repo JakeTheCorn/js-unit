505494036b5e4dfe2a3a19f7b2a10b7b1bc06038
JakeTheCorn <jakethecorn@gmail.com>
2020-08-28 13:55:38 -0400

    add years_equal function to datetime

--- lib_tests/datetime.test.js
+++ lib_tests/datetime.test.js
@@ -7,6 +7,16 @@ class DateTime {
         this.__throw_for_invalid_fields()
     }
 
+    years_equal(other) {
+        if (!(other instanceof DateTime)) {
+            throw new TypeError('years_equal must be called with instance of DateTime')
+        }
+        const other_full_year = other.date.getFullYear()
+        const this_full_year = this.date.getFullYear()
+
+        return other_full_year === this_full_year
+    }
+
     __throw_for_invalid_fields() {
         if (!(this.date instanceof Date)) {
             throw new TypeError()
@@ -16,16 +26,64 @@ class DateTime {
 
 
 class DateTimeTests extends unittest.TestCase {
-    test_it_cannot_be_constructed_without_constructor_args() {
+    test_it_can_be_constructed_with_date() {
+        new DateTime(new Date())
+    }
+
+    test_it_cannot_be_constructed_without_date() {
         this.assertRaises(TypeError, () => {
-            const dt = new DateTime()
+            new DateTime('')
         })
     }
 
-    test_it_can_be_constructed_with_date() {
-        new DateTime(new Date())
+    test_years_equal_pass() {
+        const year2020 = 2020
+        const d1 = new Date()
+        const d2 = new Date()
+        d1.setFullYear(year2020)
+        d2.setFullYear(year2020)
+        const dt = new DateTime(d1)
+        const dt2 = new DateTime(d2)
+
+        const years_equal = dt.years_equal(dt2)
+        this.assertIsTrue(years_equal)
+    }
+
+    test_years_equal_TypeError_thrown() {
+        this.assertRaises(TypeError, () => {
+            const year2020 = 2020
+            const date1 = new Date(year2020)
+            const dt = new DateTime(date1)
+            dt.years_equal('')
+        })
+    }
+
+    test_years_equal_returns_false_when_years_not_equal() {
+        const year2020 = 2020
+        const year2021 = 2021
+        const d1 = new Date()
+        const d2 = new Date()
+        d1.setFullYear(year2020)
+        d2.setFullYear(year2021)
+        const dt1 = new DateTime(d1)
+        const dt2 = new DateTime(d2)
+
+        const years_equal = dt1.years_equal(dt2)
+        this.assertIsFalse(years_equal)
     }
 }
 
 
 unittest.register(DateTimeTests).run_if_main(module)
+
+/*
+class TableTestCase extends unittest.TestCase {
+
+    items = []
+
+    assert(item) {
+        this.
+    }
+}
+
+*/
